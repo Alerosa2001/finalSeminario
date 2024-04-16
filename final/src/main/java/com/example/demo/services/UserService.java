bd4d22392508f0f2cde41ef6735b503d7c3058bc
Alerosa <alejandrorosa2001@gmail.com>
2024-04-16 17:14:39 -0300

    correcion request put

--- final/src/main/java/com/example/demo/services/UserService.java
+++ final/src/main/java/com/example/demo/services/UserService.java
@@ -1,10 +1,11 @@
 package com.example.demo.services;
 
+import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.models.UserModel;
 import com.example.demo.repositories.IUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.exception.ResourceNotFoundException;
 import java.util.ArrayList;
 import java.util.Optional;
 
@@ -26,14 +27,21 @@ public class UserService {
         return userRepository.findById(id);
     }
 
-    public UserModel updateById(UserModel  request, Long id ){
-        UserModel user = userRepository.findById(id).get();
+    public UserModel updateById(UserModel request, Long id) {
+        Optional<UserModel> optionalUser = userRepository.findById(id);
 
-        user.setFirstName(request.getFirstName());
-        user.setLastName(request.getLastName());
-        user.setEmail(request.getEmail());
+        if (optionalUser.isPresent()) {
+            UserModel user = optionalUser.get();
 
-        return user;
+            user.setFirstName(request.getFirstName());
+            user.setLastName(request.getLastName());
+            user.setEmail(request.getEmail());
+
+            return userRepository.save(user); // Guardar los cambios en la base de datos
+        } else {
+
+            throw new ResourceNotFoundException("User not found with id: " + id);
+        }
     }
 
     public Boolean deleteUser (Long id) {
